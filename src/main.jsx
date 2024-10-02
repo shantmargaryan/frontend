6a85ccfc441116723e517eb6c60d3cd49403ff30
Shant <shant212205@gmail.com>
2024-10-02 10:01:18 -0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import "./sass/index.scss"
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom"
 import App from './App.jsx'
+import ScrollToTop from "./Hooks/ScrollToTop.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
+        <ScrollToTop />
         <App />
     </Router>
 )
